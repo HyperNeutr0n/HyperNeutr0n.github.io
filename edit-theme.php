8160c30f4b02608a2ea70f9875522d090db5acb7
HyperNeutr0n <randylemuel21@gmail.com>
2024-04-05 09:27:05 +0700

    Base

---
+++ edit-theme.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Theme</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <label for="name">Name of your theme :</label>
+        <input type="text" name="name" id="" required>
+        <br>
+        <br>
+
+        <label for="bg-color">Color of Page Background :</label>
+        <input type="color" name="bg-color" id="" required>
+        <br>
+        <br>
+
+        <label for="heading-color">Color of Heading 1 :</label>
+        <input type="color" name="h1-color" id="" required>
+        <br>
+        <br>
+
+        <label for="allignment">Allignment of Heading 1 :</label>
+        <select name="allignment" id="" required>
+            <option value="default" disabled selected>-- Choose the Allignment</option>
+            <option value="left">Left</option>
+            <option value="center">Center</option>
+            <option value="right">Right</option>
+        </select>
+        <br>
+        <br>
+
+        <label for="par-color">Color of Paragraph :</label>
+        <input type="color" name="par-color" id="" required>
+        <br>
+        <br>
+
+        <label for="par-size">Font Size of Paragraph :</label>
+        <input type="number" name="par-size" id="" required>
+        <br>
+        <br>
+        <input type="submit" value="Save">
+    </form>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Tengah Senester</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action="" method="post">
+            <label for="theme">Theme : </label>
+            <select name="theme" id="theme" required>
+                <option value="" disabled selected>-- Choose Theme --</option>
+                <option value="">Biru</option>
+            </select>
+            <a href="./add-theme.php">Add New Theme</a>
+            <br>
+            <br>
+            <input type="submit" value="Choose the Theme" formaction="">
+            <input type="submit" value="Edit the Theme" formaction="edit-theme.php">
+        </form>
+        <hr>
+
+        <h1>Heading 1</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eaque aut debitis harum autem quisquam provident fuga nostrum non maiores facilis voluptatibus voluptas dolor, nisi similique qui itaque molestias dolorum ut temporibus molestiae ullam? Iure labore officia beatae nostrum veritatis repellendus enim sapiente sunt obcaecati minus voluptates rem animi fuga eaque perspiciatis cupiditate, asperiores explicabo deserunt natus. Laboriosam reprehenderit quaerat quis architecto sunt. Facere, alias eius veniam harum odit maiores quis at rem quasi, accusantium optio culpa vero adipisci repudiandae quidem ducimus accusamus vitae consequatur dolorum, repellat officiis quaerat! Obcaecati tenetur nulla facilis, voluptate dolor consequatur a optio incidunt quidem?</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia odio, corporis soluta itaque nostrum atque asperiores placeat quis labore inventore corrupti, in illum vero quae non. Quisquam nam quam pariatur exercitationem sunt obcaecati eum voluptatum sequi, fugit inventore, explicabo vel, nobis numquam vero quasi illo perspiciatis. Ipsam impedit aut fugiat deserunt nihil eaque praesentium illo, eius natus pariatur quis, sequi quae reiciendis animi odit corporis quaerat amet iure architecto aliquam. Eaque rerum eum magni! Officiis accusamus incidunt nobis veniam ipsum, nihil voluptatem, commodi tempore corrupti nisi molestias suscipit quis impedit nostrum numquam ducimus itaque porro cum dolorem perferendis assumenda eveniet?</p>
+    </div>
+</body>
+
+</html>
