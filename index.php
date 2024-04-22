a5ce40df25aa085438dcadd7e69f9a1061bf8eb6
hyperneutr0n <randylemuel21@gmail.com>
2024-04-22 16:24:33 +0700

    Final final commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project Tengah Senester</title>
+    <title>Project Tengah Semester</title>
 
     <?php
     // Load semua theme dari cookie
